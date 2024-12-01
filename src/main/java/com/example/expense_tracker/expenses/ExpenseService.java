bb7e7333871bcdd6c2118af549cfc9047d376788
Abhay Mutalikdesai <abhaymutalikdesai6@gmail.com>
2024-12-01 18:39:55 +0530

    Add validation for category in expense save

--- src/main/java/com/example/expense_tracker/expenses/ExpenseService.java
+++ src/main/java/com/example/expense_tracker/expenses/ExpenseService.java
@@ -1,5 +1,6 @@
 package com.example.expense_tracker.expenses;
 
+import com.example.expense_tracker.categories.CategoryService;
 import com.example.expense_tracker.utility.APIResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,17 @@ import java.util.List;
 public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
+    private final CategoryService categoryService;
 
-    public ExpenseService(ExpenseRepository expenseRepository) {
+    public ExpenseService(ExpenseRepository expenseRepository, CategoryService categoryService) {
         this.expenseRepository = expenseRepository;
+        this.categoryService = categoryService;
     }
 
     public APIResponse createExpense(ExpenseModel expense) {
+        if (!categoryService.isCategoryExists(expense.getCategory()))
+            throw new RuntimeException("Category " + expense.getCategory() + " does not exists.");
+
         ExpenseModel newExpense = expenseRepository.save(expense);
         return new APIResponse(newExpense.getId().toString(), "Created");
     }
@@ -24,6 +30,9 @@ public class ExpenseService {
     public APIResponse updateExpense(Integer expenseId, ExpenseModel expense) {
         ExpenseModel existingExpense = expenseRepository.findById(expenseId)
                 .orElseThrow(() -> new RuntimeException("Expense not found with id: " + expenseId));
+
+        if (categoryService.isCategoryExists(expense.getCategory()))
+            throw new RuntimeException("Category " + expense.getCategory() + " does not exists.");
 
         existingExpense.setTitle(expense.getTitle());
         existingExpense.setAmount(expense.getAmount());
